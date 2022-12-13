de7b4032a49aca53f5021c1e0bd331b7f64c697b
johansmarius <jsmarius@hotmail.com>
2022-12-13 21:41:55 +0100

    Removed old files

--- Domain.Services/IFamilyRepository.cs
+++
@@ -1,6 +0,0 @@
-namespace Domain.Services;
-
-public interface IFamilyRepository
-{
-    Family GetFamilyByNameAndAddress(string name, string address);
-}
--- DomainServicesXUnit.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace DomainServicesXUnit.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
